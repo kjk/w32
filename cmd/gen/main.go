48195421fb2713082af764a1c2dfdf8e08dfe11f
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2024-05-09 15:12:37 +0200

    add -parse-sys

--- cmd/gen/main.go
+++ cmd/gen/main.go
@@ -183,7 +183,6 @@ func writeStringBuilderToFile(b *strings.Builder, path string) {
 func dumpFunctions() {
 	logf("dumpFunctions\n")
 	var byNamespace strings.Builder
-	var byDll strings.Builder
 	f := loadWinMetadata()
 	c, err := NewContext(f)
 	panicIfErr(err)
@@ -213,31 +212,169 @@ func dumpFunctions() {
 			addToMapStringArray(funcsByDll, dllName, funcName)
 		}
 	}
+	var byDll strings.Builder
+	nTotalFuncs := genDllFuncsInfo(&byDll, funcsByDll)
+	writeStringBuilderToFile(&byNamespace, "funcs_by_namespace.txt")
+	writeStringBuilderToFile(&byDll, "funcs_by_dll.txt")
+	logf("total functions: %d in %d dlls\n", nTotalFuncs, len(funcsByDll))
+}
+
+// TODO: should allow additional info, like a comment
+func genDllFuncsInfo(b *strings.Builder, funcsByDll map[string][]string) int {
 	nTotalFuncs := 0
 	dllNames := maps.Keys(funcsByDll)
 	slices.Sort(dllNames)
 	for _, dllName := range dllNames {
 		funcs := funcsByDll[dllName]
 		slices.Sort(funcs)
-		logbf(&byDll, "%s\n", dllName)
+		logbf(b, "%s\n", dllName)
 		for _, s := range funcs {
-			logbf(&byDll, "  %s\n", s)
+			logbf(b, "  %s\n", s)
 		}
 		nTotalFuncs += len(funcs)
 	}
-	writeStringBuilderToFile(&byNamespace, "funcs_by_namespace.txt")
-	writeStringBuilderToFile(&byDll, "funcs_by_dll.txt")
-	logf("total functions: %d in %d dlls\n", nTotalFuncs, len(dllNames))
+	return nTotalFuncs
+}
+
+// returns full paths of files starting in root
+func getFilesRecur(root string, fnInclude func(name, path string) bool) ([]string, error) {
+	var res []string
+	dirsToVisit := []string{root}
+	for len(dirsToVisit) > 0 {
+		dir := dirsToVisit[0]
+		dirsToVisit = dirsToVisit[1:]
+		files, err := os.ReadDir(dir)
+		if err != nil {
+			return res, err
+		}
+		for _, fi := range files {
+			name := fi.Name()
+			path := filepath.Join(dir, name)
+			if fi.IsDir() {
+				dirsToVisit = append(dirsToVisit, path)
+			} else if fi.Type().IsRegular() {
+				if fnInclude == nil || fnInclude(name, path) {
+					res = append(res, path)
+				}
+			}
+		}
+	}
+	return res, nil
+}
+
+func getGoFilesRecur(root string) ([]string, error) {
+	fnInclude := func(name, path string) bool {
+		// TODO: could be faster
+		name = strings.ToLower(name)
+		return strings.HasSuffix(name, ".go")
+	}
+	return getFilesRecur(root, fnInclude)
+}
+
+// parse: wtsapi32.WTSFreeMemory
+func getDllFuncName(s string) (string, string) {
+	parts := strings.Split(s, ".")
+	panicIf(len(parts) > 2, "unexpected name '%s'", s)
+	if len(parts) == 1 {
+		return s, "kernel32.dll"
+	}
+	dllName := strings.TrimSpace(parts[0])
+	dllName = strings.ToLower(dllName)
+	if !strings.Contains(dllName, ".") {
+		dllName += ".dll"
+	}
+	name := strings.TrimSpace(parts[1])
+	return name, dllName
+}
+
+type ParsedSys struct {
+	Dll         string
+	DllFuncName string
+	GoFuncName  string
+}
+
+/*
+Parse this format:
+//sys WTSFreeMemory(ptr uintptr) = wtsapi32.WTSFreeMemory
+*/
+func parseMaybeSysLine(s string) *ParsedSys {
+	if !strings.HasPrefix(s, "//sys") {
+		return nil
+	}
+	logf("%s\n", s)
+	s2 := s[5:]
+	s2 = strings.TrimSpace(s2)
+	// logf("  %s\n", s2)
+	parts := strings.Split(s2, "(")
+	funcName, dllName := getDllFuncName(parts[0])
+	goName := funcName
+	s3 := strings.ReplaceAll(s2, "==", "")
+	s3 = strings.ReplaceAll(s3, "!=", "")
+	s3 = strings.ReplaceAll(s3, "<=", "")
+	parts = strings.Split(s3, "=")
+	panicIf(len(parts) > 2, "unexpecpted line: '%s'", s)
+	if len(parts) == 2 {
+		funcName, dllName = getDllFuncName(parts[1])
+	}
+	logf("  %s.%s => %s\n", dllName, funcName, goName)
+	return &ParsedSys{
+		Dll:         dllName,
+		DllFuncName: funcName,
+		GoFuncName:  goName,
+	}
+}
+
+func extractSysDeclarations(path string) []*ParsedSys {
+	var res []*ParsedSys
+	// logf("file: %s\n", path)
+	lines, err := u.ReadLines(path)
+	panicIfErr(err)
+	for _, s := range lines {
+		p := parseMaybeSysLine(s)
+		if p != nil {
+			res = append(res, p)
+		}
+	}
+	return res
+}
+
+// parse checkout of golang.org/x/sys repo to figure out
+// wihch functions are already implemented by
+// golang.org/x/sys/windows (we don't want to duplicate those)
+func genImplementedBySysXWindows() {
+	dir := filepath.Join("..", "sys", "windows")
+	goFiles, err := getGoFilesRecur(dir)
+	if err != nil {
+		logf("We expect checkout of golang.org/x/sys repository to our sibling\n")
+		logf("To checkout: git clone https://go.googlesource.com/sys")
+		panicIfErr(err)
+	}
+
+	var a []*ParsedSys
+	for _, path := range goFiles {
+		a = append(a, extractSysDeclarations(path)...)
+	}
+
+	funcsByDll := map[string][]string{}
+	for _, p := range a {
+		addToMapStringArray(funcsByDll, p.Dll, p.DllFuncName)
+	}
+
+	var b strings.Builder
+	genDllFuncsInfo(&b, funcsByDll)
+	writeStringBuilderToFile(&b, "implemented_in_sys.txt")
 }
 
 func main() {
 	var (
 		flgDumpFunctions bool
 		flgGen           bool
+		flgParseSys      bool
 	)
 	{
 		flag.BoolVar(&flgDumpFunctions, "dump-functions", false, "dump information about all functions")
 		flag.BoolVar(&flgGen, "gen", false, "generate")
+		flag.BoolVar(&flgParseSys, "parse-sys", false, "parse golang/x/sys/windows and generate implemented_in_sys.txt")
 		flag.Parse()
 	}
 
@@ -249,9 +386,16 @@ func main() {
 		dumpFunctions()
 		return
 	}
+
 	if flgGen {
 		genFromWinmd()
 		return
 	}
+
+	if flgParseSys {
+		genImplementedBySysXWindows()
+		return
+	}
+
 	flag.Usage()
 }
